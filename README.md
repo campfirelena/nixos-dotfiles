07a2abd43c867c2ee723d1755e940e49b0871895
Elena <145106804+campfirelena@users.noreply.github.com>
2025-04-02 14:52:48 +0800

    settings.nix should be finally fixed, hopefully README is too

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # nixos-dotfiles
+
 The folders have specific purposes. 
+
 Config holds all the stuff that would be symlinked into $HOME/.config/ 
+
 Hosts are the machines that will run on this config. To add a machine, just add a folder and a short config to turn the modules on and off. 
-Modules are program-specific configs for the system level. The hosts directory links here. 
+
+Modules are program-specific configs for the system level. The hosts directory links here.
+
 Pkgs are program-specific configs for the user level. The users directory links here. 
+
 Users is all the users that can be on any machine. To add a user, add a folder and default.nix and home.nix. I might change this to a different setup so that users can change their stuff without needing root/sudo access. 
--- users/elena/settings.nix
+++ users/elena/settings.nix
@@ -6,31 +6,30 @@ options = {
   default = "${hostname}";
   };
 };
-config = lib.mkIf config.userapps.enable { lib.mkMerge [
-  lib.mkIf (userapps.host == "server") {
+config = lib.mkIf config.userapps.enable (lib.mkMerge [
+  (lib.mkIf (userapps.host == "server") {
     awesome.enable = false;
     firefox.enable = false;
     kitty.enable = false;
     neovim.enable = false;
     spicetify.enable = false;
     steam.enable = false;
-  };
-  lib.mkIf (userapps.host == "desktop") {
+  })
+  (lib.mkIf (userapps.host == "desktop") {
+    awesome.enable = true;
+    firefox.enable = true;
+    kitty.enable = true;
+    neovim.enable = true;
+    spicetify.enable = true;
+    steam.enable = false;
+  })
+  (lib.mkIf (userapps.host == "gaming") {
     awesome.enable = true;
     firefox.enable = true;
     kitty.enable = true;
     neovim.enable = true;
     spicetify.enable = true;
     steam.enable = true;
-  };
-  lib.mkIf (userapps.host == "testing") {
-    awesome.enable = false;
-    firefox.enable = false;
-    kitty.enable = false;
-    neovim.enable = true;
-    spicetify.enable = false;
-    steam.enable = false;
-  };
-];
-};
+  })
+]);
 }
