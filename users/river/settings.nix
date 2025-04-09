da9fda6e812309d762e8bc5ebe57467dbdf285fc
Elena <145106804+campfirelena@users.noreply.github.com>
2025-04-09 15:23:04 +0800

    added new packages to settings for both users

--- users/river/settings.nix
+++ users/river/settings.nix
@@ -10,24 +10,33 @@ config = lib.mkIf config.userapps.enable (lib.mkMerge [
   (lib.mkIf (userapps.host == "server") {
     awesome.enable = false;
     firefox.enable = false;
+    flatpak.enable = false;
     kitty.enable = false;
+    modman.enable = false;
     neovim.enable = false;
+    obsidian.enable = false;
     spicetify.enable = false;
     steam.enable = false;
   })
   (lib.mkIf (userapps.host == "desktop") {
     awesome.enable = true;
     firefox.enable = true;
+    flatpak.enable = false
     kitty.enable = true;
+    modman.enable = false;
     neovim.enable = true;
+    obsidian.enable = true;
     spicetify.enable = true;
     steam.enable = false;
   })
   (lib.mkIf (userapps.host == "gaming") {
     awesome.enable = true;
     firefox.enable = true;
+    flatpak.enable = true;
     kitty.enable = true;
+    modman.enable = true;
     neovim.enable = true;
+    obsidian.enable = false;
     spicetify.enable = true;
     steam.enable = true;
   })
